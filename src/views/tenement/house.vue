9de10bb13f977691a60da995f35d6015809bee58
wjl <1405720461@qq.com>
2023-07-15 10:17:36 +0800

    1

---
+++ src/views/tenement/house.vue
@@ -0,0 +1,24 @@
+﻿<template>
+  <div class=''></div>
+</template>
+
+<script>
+export default {
+  name: '',
+  props: [],
+  data () {
+    return {
+      
+    }
+  },
+  mounted () {
+    
+  },
+  methods: {
+
+  }
+}
+</script>
+<style scoped>
+
+</style>
